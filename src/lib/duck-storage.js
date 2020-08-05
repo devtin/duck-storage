e34ceb2d1f81e1a23d01fe27a400f7f8fae66cc4
Martin Rafael Gonzalez <tin@devtin.io>
2020-08-05 16:55:49 -0400

    add plugins

--- src/lib/duck-storage.js
+++ src/lib/duck-storage.js
@@ -1,11 +1,30 @@
+import loadReference from './plugins/load-reference'
+
 const store = Object.create(null)
+const plugins = [loadReference]
 export const DuckStorage = {
+  plugin (fn) {
+    plugins.push(fn)
+  },
   registerRack (duckRack) {
     if (store[duckRack.name]) {
       throw new Error(`a DuckRack with the name ${duckRack.name} is already registered`)
     }
 
     store[duckRack.name] = duckRack
+    plugins.forEach(fn => {
+      fn({ DuckStorage, duckRack })
+    })
+  },
+  removeRack (rackName) {
+    if (!store[rackName]) {
+      throw new Error(`a DuckRack with the name ${rackName} could not be found`)
+    }
+
+    delete store[rackName]
+  },
+  listRacks () {
+    return Object.keys(store)
   },
   getRackByName (rackName) {
     return store[rackName]
---
+++ src/lib/plugins/load-reference.js
@@ -0,0 +1,28 @@
+import { Utils } from '@devtin/schema-validator'
+import set from 'lodash/set'
+
+export default function ({ DuckStorage, duckRack }) {
+  async function loadReferences (entry) {
+    const entriesToLoad = this.duckModel
+      .schema
+      .paths
+      .filter((path) => {
+        return this.duckModel.schema.schemaAtPath(path).settings.duckRack && Utils.find(entry, path)
+      })
+      .map(path => {
+        const Rack = DuckStorage.getRackByName(this.duckModel.schema.schemaAtPath(path).settings.duckRack)
+        const _idPayload = Utils.find(entry, path)
+        const _id = Rack.duckModel.schema.isValid(_idPayload) ? _idPayload._id : _idPayload
+        return { duckRack: this.duckModel.schema.schemaAtPath(path).settings.duckRack, _id, path }
+      })
+
+    for (const entryToLoad of entriesToLoad) {
+      set(entry, entryToLoad.path, await DuckStorage.getRackByName(entryToLoad.duckRack).findOneById(entryToLoad._id))
+    }
+
+    return entry
+  }
+
+  duckRack.hook('after', 'read', loadReferences)
+  duckRack.hook('after', 'create', loadReferences)
+}

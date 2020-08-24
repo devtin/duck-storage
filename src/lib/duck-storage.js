1ee70cbcdfe77d64ebd5c906753055e727509ec3
Martin Rafael Gonzalez <tin@devtin.io>
2020-08-24 17:15:20 -0400

    fix(registerRack): return duckRack

--- src/lib/duck-storage.js
+++ src/lib/duck-storage.js
@@ -68,6 +68,7 @@ export class DuckStorageClass extends EventEmitter {
       fn({ DuckStorage: this, duckRack })
     })
     this._wireRack(duckRack)
+    return duckRack
   }
 
   removeRack (rackName) {

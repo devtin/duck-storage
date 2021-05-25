cc114779ed570ed179cca6a9b986903fb1ae3d33
Martin Rafael Gonzalez <tin@devtin.io>
2021-05-25 13:20:36 -0400

    feat: remap fn callback

--- src/lib/register-duck-racks-from-dir.js
+++ src/lib/register-duck-racks-from-dir.js
@@ -1,8 +1,8 @@
 import { jsDirIntoJson } from 'js-dir-into-json'
 import { registerDuckRacksFromObj } from './register-duck-racks-from-obj'
 
-export async function registerDuckRacksFromDir (duckStorage, directory) {
-  return registerDuckRacksFromObj(duckStorage, await jsDirIntoJson(directory, {
+export async function registerDuckRacksFromDir (duckStorage, directory, remap = (obj) => obj) {
+  return registerDuckRacksFromObj(duckStorage, remap(await jsDirIntoJson(directory, {
     extensions: ['!lib', '!__tests__', '!*.unit.js', '!*.spec.js', '!*.test.js', '*.js', '*.mjs']
-  }))
+  })))
 }
